1d0282610aaaa9a018570b39fe1b40b10b2e7dce
leventezhu <30834450+leventezhu@users.noreply.github.com>
2023-04-13 22:02:41 +0200

    Implement new,load,save,exit command events

--- WpfApp1/ViewModel/MaynotViewModel.cs
+++ WpfApp1/ViewModel/MaynotViewModel.cs
@@ -15,6 +15,98 @@ namespace Maynot.WPF.ViewModel
         public MaynotViewModel()
         {
             Debug.WriteLine("Instantia");
+
+            // parancsok kezelése
+            NewGameCommand = new DelegateCommand(param => OnNewGame());
+            LoadGameCommand = new DelegateCommand(param => OnLoadGame());
+            SaveGameCommand = new DelegateCommand(param => OnSaveGame());
+            ExitCommand = new DelegateCommand(param => OnExitGame());
         }
+
+        #region Properties
+
+        /// <summary>
+        /// Új játék kezdése parancs lekérdezése.
+        /// </summary>
+        public DelegateCommand NewGameCommand { get; private set; }
+
+        /// <summary>
+        /// Játék betöltése parancs lekérdezése.
+        /// </summary>
+        public DelegateCommand LoadGameCommand { get; private set; }
+
+        /// <summary>
+        /// Játék mentése parancs lekérdezése.
+        /// </summary>
+        public DelegateCommand SaveGameCommand { get; private set; }
+
+        /// <summary>
+        /// Kilépés parancs lekérdezése.
+        /// </summary>
+        public DelegateCommand ExitCommand { get; private set; }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Új játék eseménye.
+        /// </summary>
+        public event EventHandler? NewGame;
+
+        /// <summary>
+        /// Játék betöltésének eseménye.
+        /// </summary>
+        public event EventHandler? LoadGame;
+
+        /// <summary>
+        /// Játék mentésének eseménye.
+        /// </summary>
+        public event EventHandler? SaveGame;
+
+        /// <summary>
+        /// Játékból való kilépés eseménye.
+        /// </summary>
+        public event EventHandler? ExitGame;
+
+        #endregion
+
+        #region Event methods
+
+        /// <summary>
+        /// Új játék indításának eseménykiváltása.
+        /// </summary>
+        private void OnNewGame()
+        {
+            NewGame?.Invoke(this, EventArgs.Empty);
+        }
+
+
+
+        /// <summary>
+        /// Játék betöltése eseménykiváltása.
+        /// </summary>
+        private void OnLoadGame()
+        {
+            LoadGame?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Játék mentése eseménykiváltása.
+        /// </summary>
+        private void OnSaveGame()
+        {
+            SaveGame?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Játékból való kilépés eseménykiváltása.
+        /// </summary>
+        private void OnExitGame()
+        {
+            ExitGame?.Invoke(this, EventArgs.Empty);
+        }
+
+        #endregion
     }
 }
