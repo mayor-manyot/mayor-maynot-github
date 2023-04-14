cd00e631071bde65b6e9bbb9cefa9a45704f0705
Szabó Zsombor Balázs <g3skpf@inf.elte.hu>
2023-04-14 10:09:11 +0200

    Zone MaynotTile leszármazása és annak típus felsorolója

---
+++ WpfApp1/ViewModel/Zone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maynot.WPF.ViewModel
+{
+    public class Zone : MaynotTile
+    {
+        private Int32 _capacity;
+        private Int32 _currentPopulation;
+        public int CurrentPopulation { get { return _currentPopulation; } set { _currentPopulation = value; } }
+        public Int32 Capacity { get { return _capacity; } set { _capacity = value; } }
+        public ZoneType Type { get; set; }
+    }
+
+    public enum ZoneType
+    {
+        RESIDENTIAL,
+        INDUSTRIAL,
+        SERVICE,
+        EMPTY,
+    }
+}
