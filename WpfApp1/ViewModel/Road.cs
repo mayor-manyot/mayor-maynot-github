70706635e7f8d4aa808ac2501b2afb8978c317b1
Szabó Zsombor Balázs <g3skpf@inf.elte.hu>
2023-04-14 10:24:26 +0200

    Erdő és Út MaynotTile altípust

---
+++ WpfApp1/ViewModel/Road.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Maynot.WPF.ViewModel
+{
+    public class Road
+    {
+        private Int32 _buildCost;
+        public Int32 BuildCost { get { return _buildCost; } set { _buildCost = value; } }
+        public Road(int buildCost)
+        {
+            BuildCost = buildCost;
+        }
+    }
+}
--- WpfApp1/ViewModel/Zone.cs
+++ WpfApp1/ViewModel/Zone.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,12 @@ namespace Maynot.WPF.ViewModel
         public int CurrentPopulation { get { return _currentPopulation; } set { _currentPopulation = value; } }
         public Int32 Capacity { get { return _capacity; } set { _capacity = value; } }
         public ZoneType Type { get; set; }
+        public Zone(int capacity, int currentPopulation, ZoneType type)
+        {
+            Capacity = capacity;
+            CurrentPopulation = currentPopulation;
+            Type = type;
+        }
     }
 
     public enum ZoneType
