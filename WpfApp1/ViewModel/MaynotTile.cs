cf2a10fbc20a1d257832cc6bee4e67ada452c536
Szabó Zsombor Balázs <g3skpf@inf.elte.hu>
2023-04-14 08:19:50 +0200

    játéktábla mezői

---
+++ WpfApp1/ViewModel/MaynotTile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maynot.WPF.ViewModel
+{
+    public class MaynotTile : ViewModelBase
+    {
+        private String _name;
+        public String Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Int32 X { get; set; }
+        public Int32 Y { get; set; }
+        public DelegateCommand? ClickCommand { get; set; }
+    }
+}
